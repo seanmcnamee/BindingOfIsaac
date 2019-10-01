ccd45b9c8bcc1971d190449f23b71340547cbba3
seanmcnamee <seanmcnamee.45@gmail.com>
2019-10-01 17:49:13 -0400

    basic Game Class Layout

--- src/app/gameclasses/GameBar.java
+++ src/app/gameclasses/GameBar.java
@@ -1,21 +1,28 @@
 package app.gameclasses;
 
 import java.awt.Graphics;
+import java.awt.Color;
 
-import javax.swing.text.Position;
-
-import app.supportclasses.BufferedImageLoader;
 import app.supportclasses.GameValues;
+
 /**
  * GameBar
  */
 public class GameBar {
+    GameValues gameValues;
 
-    public GameBar() {
-
+    public GameBar(GameValues gameValues) {
+        this.gameValues = gameValues;
     }
 
     public void render(Graphics g) {
-
+        //Use these values to figure out everyting else
+        double xStart = gameValues.WIDTH_SCALE_1*(gameValues.gameScale-gameValues.GAME_BAR_WIDTH)*.5;
+        double yStart = 0;
+        double xSize = gameValues.WIDTH_SCALE_1*gameValues.gameScale*gameValues.GAME_BAR_WIDTH;
+        double ySize = gameValues.HEIGHT_SCALE_1*gameValues.gameScale*gameValues.GAME_BAR_HEIGHT;
+        
+        g.setColor(Color.gray);
+        g.fillRect((int)xStart, (int)yStart, (int)xSize, (int)ySize);
     }
 }
